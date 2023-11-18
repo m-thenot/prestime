001e94054f6f622e1594b4e0e487a8d74cbc6fc4
MartinT <martinthenot@gmail.com>
2023-11-18 21:27:16 +0300

    fix: move fb pixel init to client side

--- app/(booking)/booking/layout.tsx
+++ app/(booking)/booking/layout.tsx
@@ -6,7 +6,6 @@ import { BookingProvider } from "@contexts/booking";
 import { defaultMetadata, defaultViewport } from "@utils/defaultMetadata";
 import { Analytics } from "@vercel/analytics/react";
 import Tracking from "@components/Tracking";
-import Script from "next/script";
 
 export default async function RootLayout({
   children,
@@ -26,24 +25,6 @@ export default async function RootLayout({
           </BookingProvider>
         </div>
       </main>
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1774956386278965');
-          fbq('track', 'PageView');
-          `,
-        }}
-      />
     </LayoutContainer>
   );
 }
